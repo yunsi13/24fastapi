a7cfa124021859bd31d155a465c1b648074e3868
yunsi13 <diddiddbs@naver.com>
2024-01-08 22:53:50 +0900

    id added

--- fist/main.py
+++ fist/main.py
@@ -5,7 +5,15 @@ from fastapi.responses import FileResponse
 app = FastAPI()
 
 @app.get("/")
+def index():
+    return {'data' : 'green'}
+
+@app.get("/green/{id}")
+def show(id):
+    return {'green' : id}
+
+@app.get("/green")
 def read_root():
     return FileResponse('fist/green.html')
 
-#uvicorn main:app --reload
+#uvicorn fist.main:app --reload

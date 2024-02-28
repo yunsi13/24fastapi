3d0c4c75a3d7f441a0758414e1d7aca885e95cc9
yunsi13 <diddiddbs@naver.com>
2024-02-28 16:25:53 +0900

    board html added

--- fist/main.py
+++ fist/main.py
@@ -8,12 +8,18 @@ app = FastAPI()
 def index():
     return {'data' : 'green'}
 
-@app.get("/green/{id}")
-def show(id):
-    return {'green' : id}
-
 @app.get("/green")
 def read_root():
     return FileResponse('fist/green.html')
+
+@app.get("/green/unpublished")
+def unpublished():
+    return {'data':'all unpublished green..'}
+
+@app.get("/green/{id}")
+def show(id : int):
+    return {'green' : id}
+
+
 
 #uvicorn fist.main:app --reload
